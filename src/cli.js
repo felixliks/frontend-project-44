001513b70cbe3937888b2a6f8edf9bab4b825837
maksim <nemcenkoa474@gmail.com>
2025-06-10 16:20:41 +0300

    sh

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-export const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
+function greetUser () {
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+}
 
-export const askQuestion = (question) => {
-  console.log(`Question: ${question}`);
-};
-
-export const getAnswer = () => {
-  return readlineSync.question('Your answer: ').toLowerCase().trim();
-};
+export default greetUser
